f3702f44ee5d8eeb3eb69da96a3ecf9184ec4af8
Jordi Prats <jprats@systemadmin.es>
2016-11-18 12:45:34 +0100

    job

--- manifests/scheduledrsync.pp
+++ manifests/scheduledrsync.pp
@@ -1,3 +1,36 @@
-define rsync::scheduledrsync() {
-  
+define rsync::scheduledrsync(
+                              $origin,
+                              $destination,
+                              $hour        = '0',
+                              $minute      = '0',
+                              $month       = undef,
+                              $monthday    = undef,
+                              $weekday     = undef,
+                              $ensure      = 'present',
+                              $cronjobname = undef,
+                              $ionice      = true,
+                              $user        = 'root',
+                            ) {
+
+  if($cronjobname!=undef)
+  {
+    $cron_job_name=$cronjobname
+  }
+  else
+  {
+    $cron_job_name="cronjob rsync ${name}"
+  }
+
+  #"find ${path} ${type} -mtime ${mtime} ${action}"
+  cron { $cron_job_name:
+    ensure   => $ensure,
+    command  => inline_template('<% if @ionice %>ionice -c2 -n2 <% end %>rsync -a -H -x --numeric-ids @origin @destination'),
+    user     => $user,
+    hour     => $hour,
+    minute   => $minute,
+    month    => $month,
+    monthday => $monthday,
+    weekday  => $weekday,
+  }
+
 }
