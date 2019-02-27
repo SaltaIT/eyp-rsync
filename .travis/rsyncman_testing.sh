14ccc552ed4c06a3dc12898398d408620e85cd34
Jordi Prats <jordi.prats@gmail.com>
2019-02-27 17:38:08 +0100

    mes checks

--- .travis/rsyncman_testing.sh
+++ .travis/rsyncman_testing.sh
@@ -30,6 +30,16 @@ then
   TEST0_2="ok"
 fi
 
+if [ -f "${DIR_ORIGIN}/.snapshot" ];
+then
+  TEST0_3="ok"
+fi
+
+if [ ! -f "${DIR_DESTINATION}/.snapshot" ];
+then
+  TEST0_4="ok"
+fi
+
 python /home/travis/build/jordiprats/eyp-rsync/files/rsyncman.py -c /home/travis/build/jordiprats/eyp-rsync/.travis/localrsync.config -d
 
 echo "================"
@@ -52,6 +62,16 @@ then
   TEST1_2="ok"
 fi
 
+if [ -f "${DIR_ORIGIN}/.snapshot" ];
+then
+  TEST1_3="ok"
+fi
+
+if [ ! -f "${DIR_DESTINATION}/.snapshot" ];
+then
+  TEST1_4="ok"
+fi
+
 python /home/travis/build/jordiprats/eyp-rsync/files/rsyncman.py -c /home/travis/build/jordiprats/eyp-rsync/.travis/localrsync.config
 
 echo "==============="
@@ -72,6 +92,16 @@ fi
 if [ ! -f "${DIR_DESTINATION}/file_to_be_removed" ];
 then
   TEST2_2="ok"
+fi
+
+if [ -f "${DIR_ORIGIN}/.snapshot" ];
+then
+  TEST2_3="ok"
+fi
+
+if [ ! -f "${DIR_DESTINATION}/.snapshot" ];
+then
+  TEST2_4="ok"
 fi
 
 touch "${DIR_DESTINATION}/file_to_be_copied_back"
@@ -99,29 +129,50 @@ then
   TEST3_2="ok"
 fi
 
+if [ -f "${DIR_ORIGIN}/.snapshot" ];
+then
+  TEST3_3="ok"
+fi
+
+if [ ! -f "${DIR_DESTINATION}/.snapshot" ];
+then
+  TEST3_4="ok"
+fi
+
 echo ""
 echo "TEST 0 - check environment"
 echo "======"
 echo "TEST0_1: ${TEST0_1}"
 echo "TEST0_2: ${TEST0_2}"
+echo "TEST0_3: ${TEST0_3}"
+echo "TEST0_4: ${TEST0_4}"
 echo ""
 echo "TEST 1 - dry run"
 echo "======"
-echo "TEST1_1: ${TEST0_1}"
-echo "TEST1_2: ${TEST0_2}"
+echo "TEST1_1: ${TEST1_1}"
+echo "TEST1_2: ${TEST1_2}"
+echo "TEST1_3: ${TEST1_3}"
+echo "TEST1_4: ${TEST1_4}"
 echo ""
 echo "TEST 2 - after running rsync"
 echo "======"
-echo "TEST2_1: ${TEST1_1}"
-echo "TEST2_2: ${TEST1_2}"
+echo "TEST2_1: ${TEST2_1}"
+echo "TEST2_2: ${TEST2_2}"
+echo "TEST2_3: ${TEST2_3}"
+echo "TEST3_4: ${TEST2_4}"
 echo ""
 echo "TEST 3 - sync back"
 echo "======"
-echo "TEST3_1: ${TEST1_1}"
-echo "TEST3_2: ${TEST1_2}"
+echo "TEST3_1: ${TEST3_1}"
+echo "TEST3_2: ${TEST3_2}"
+echo "TEST3_3: ${TEST3_3}"
+echo "TEST4_4: ${TEST3_4}"
 echo ""
 
-if [ -z "${TEST0_1}" ] || [ -z "${TEST0_2}" ] || [ -z "${TEST1_1}" ] || [ -z "${TEST1_2}" ];
+if [ -z "${TEST0_1}" ] || [ -z "${TEST0_2}" ] || [ -z "${TEST0_3}" ] || [ -z "${TEST0_4}" ] || \
+    [ -z "${TEST1_1}" ] || [ -z "${TEST1_2}" ] || [ -z "${TEST1_3}" ] || [ -z "${TEST1_4}" ] || \
+    [ -z "${TEST2_1}" ] || [ -z "${TEST2_2}" ] || [ -z "${TEST2_3}" ] || [ -z "${TEST2_4}" ] || \
+    [ -z "${TEST3_1}" ] || [ -z "${TEST3_2}" ] || [ -z "${TEST3_3}" ] || [ -z "${TEST3_4}" ];
 then
   echo "FOUND ERRORS"
   exit 1
