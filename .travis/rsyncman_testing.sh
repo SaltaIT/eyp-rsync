6da85e2e0f0900aa88973bd2d49d2f91848245b0
Jordi Prats <jordi.prats@gmail.com>
2019-02-27 13:26:06 +0100

    rsyncman

--- .travis/rsyncman_testing.sh
+++ .travis/rsyncman_testing.sh
@@ -7,4 +7,12 @@ mkdir -p /var/log/rsyncman
 
 echo TESTING BASH
 
+python /home/travis/build/jordiprats/eyp-rsync/files/rsyncman.py -c /home/travis/build/jordiprats/eyp-rsync/.travis/localrsync.config
+
+echo /home/travis/build/jordiprats/eyp-rsync/.travis/origin
+ls -la /home/travis/build/jordiprats/eyp-rsync/.travis/origin
+
+echo /home/travis/build/jordiprats/eyp-rsync/.travis/destination
+ls -la /home/travis/build/jordiprats/eyp-rsync/.travis/destination
+
 exit 0
