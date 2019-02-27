af460eced7cbc5865789cf37eeeb5bae31337ae6
Jordi Prats <jordi.prats@gmail.com>
2019-02-27 16:44:42 +0100

    dryrun + sync back testing

--- .travis/rsyncman_testing.sh
+++ .travis/rsyncman_testing.sh
@@ -26,6 +26,18 @@ then
   TEST0_2="ok"
 fi
 
+python /home/travis/build/jordiprats/eyp-rsync/files/rsyncman.py -c /home/travis/build/jordiprats/eyp-rsync/.travis/localrsync.config -d
+
+if [ ! -f "${DIR_DESTINATION}/file_to_be_copied" ];
+then
+  TEST1_1="ok"
+fi
+
+if [ -f "${DIR_DESTINATION}/file_to_be_removed" ];
+then
+  TEST1_2="ok"
+fi
+
 python /home/travis/build/jordiprats/eyp-rsync/files/rsyncman.py -c /home/travis/build/jordiprats/eyp-rsync/.travis/localrsync.config
 
 echo $DIR_ORIGIN
@@ -36,12 +48,27 @@ ls -la $DIR_DESTINATION
 
 if [ -f "${DIR_DESTINATION}/file_to_be_copied" ];
 then
-  TEST1_1="ok"
+  TEST2_1="ok"
 fi
 
 if [ ! -f "${DIR_DESTINATION}/file_to_be_removed" ];
 then
-  TEST1_2="ok"
+  TEST2_2="ok"
+fi
+
+touch "${DIR_DESTINATION}/file_to_be_copied_back"
+touch "${DIR_ORIGIN}/file_to_be_removed"
+
+python /home/travis/build/jordiprats/eyp-rsync/files/rsyncman.py -c /home/travis/build/jordiprats/eyp-rsync/.travis/localrsync.config -b
+
+if [ ! -f "${DIR_ORIGIN}/file_to_be_removed" ];
+then
+  TEST3_1="ok"
+fi
+
+if [ -f "${DIR_ORIGIN}/file_to_be_copied_back" ];
+then
+  TEST3_2="ok"
 fi
 
 echo ""
@@ -50,10 +77,20 @@ echo "======"
 echo "TEST0_1: ${TEST0_1}"
 echo "TEST0_2: ${TEST0_2}"
 echo ""
-echo "TEST 1 - after running rsync"
+echo "TEST 1 - dry run"
 echo "======"
-echo "TEST1_1: ${TEST1_1}"
-echo "TEST1_2: ${TEST1_2}"
+echo "TEST1_1: ${TEST0_1}"
+echo "TEST1_2: ${TEST0_2}"
+echo ""
+echo "TEST 2 - after running rsync"
+echo "======"
+echo "TEST2_1: ${TEST1_1}"
+echo "TEST2_2: ${TEST1_2}"
+echo ""
+echo "TEST 3 - sync back"
+echo "======"
+echo "TEST3_1: ${TEST1_1}"
+echo "TEST3_2: ${TEST1_2}"
 echo ""
 
 if [ -z "${TEST0_1}" ] || [ -z "${TEST0_2}" ] || [ -z "${TEST1_1}" ] || [ -z "${TEST1_2}" ];
