bded6b114e4b8dc5a6d7bb79e1b0ec142dbd4218
Jordi Prats <jordi.prats@gmail.com>
2019-02-27 13:34:34 +0100

    install requirements python

--- .travis/rsyncman_testing.sh
+++ .travis/rsyncman_testing.sh
@@ -5,7 +5,7 @@
 
 mkdir -p /var/log/rsyncman
 
-echo TESTING BASH
+pip install -r /home/travis/build/jordiprats/eyp-rsync/files/requirements.txt
 
 python /home/travis/build/jordiprats/eyp-rsync/files/rsyncman.py -c /home/travis/build/jordiprats/eyp-rsync/.travis/localrsync.config
 
